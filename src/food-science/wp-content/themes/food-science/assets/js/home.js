1eeff8b951f0dac13b19122956069b9612689192
shunjimiyauchi <rio991216@gmail.com>
2026-08-27 13:18:18 +0900

    0827

--- src/food-science/wp-content/themes/food-science/assets/js/home.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($) {
-    $('.js-slider').slick({
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: false,
-        arrows: false
-    });
-});
