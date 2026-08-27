c154ed0127f20b52d7d5d71c133268259c7545e6
shunjimiyauchi <rio991216@gmail.com>
2026-08-27 13:06:22 +0900

    0827

---
+++ src/food-science/wp-content/themes/food-science/functions.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * titleタグを出力する
+ */
+add_theme_support('title-tag');
+
+/**
+ * アイキャッチ画像を使用可能にする
+ */
+add_theme_support('post-thumbnails');
+
+/**
+ * カスタムメニュー機能を使用可能にする
+ */
+add_theme_support('menus');
+
+/**
+ * titleの区切り文字を変更する
+ */
+add_filter('document_title_separator', 'my_document_title_separator');
+function my_document_title_separator(string $separator): string
+{
+    $separator = '|';
+    return $separator;
+}
+
+
+function my_dump(mixed $data): void
+{
+    echo '<pre>';
+    var_dump($data);
+    echo '</pre>';
+}
+
+/**
+ * Contact Form 7の時は整形機能をOFFにする
+ */
+add_filter('wpcf7_autop_or_not', 'my_wpcf7_autop');
+function my_wpcf7_autop()
+{
+    return false;
+}
+
+/**
+ * メインクエリを変更する
+ */
+add_action('pre_get_posts', 'my_pre_get_posts');
+function my_pre_get_posts(mixed $query)
+{
+    //管理画面、メインクエリ以外には設定しない
+    if (is_admin() || !$query->is_main_query()) {
+        return;
+    }
+
+    //トップページの場合
+    if ($query->is_home()) {
+        $query->set('posts_per_page', 3);
+        return;
+    }
+
+    // カテゴリーページの場合
+    // カスタム投稿ページの場合
+}
